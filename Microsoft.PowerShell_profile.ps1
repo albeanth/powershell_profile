c40f33eb499e74a00598724b23aafb6a03168495
aalberti <c-aalberti@terrapower.com>
2023-10-09 15:36:35 -0700

    init commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# oh-my-posh init pwsh | Invoke-Expression
+set-alias open start # creates an alias to act like bash to open file browser
+Set-PSReadLineKeyHandler -Key Tab -Function Complete # set tab complete to be bash like
+$env:VIRTUAL_ENV_DISABLE_PROMPT=1 # enable virtual env to be controlled by oh-my-posh
+Import-Module -Name Terminal-Icons # make fancy ls icons and coloring
+oh-my-posh init pwsh --config 'C:/Users/aalberti/Documents/Powershell/posh_profile.json' | Invoke-Expression
