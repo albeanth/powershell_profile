20e23d1e9e5b5f2e76f8a6b6179a0f943e36c24d
aalberti <c-aalberti@terrapower.com>
2025-05-06 23:03:00 -0700

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 # oh-my-posh init pwsh | Invoke-Expression
 set-alias open start # creates an alias to act like bash to open file browser
+new-alias ll ls
+new-alias which Get-Command
 Set-PSReadLineKeyHandler -Key Tab -Function Complete # set tab complete to be bash like
 $env:VIRTUAL_ENV_DISABLE_PROMPT=1 # enable virtual env to be controlled by oh-my-posh
 Import-Module -Name Terminal-Icons # make fancy ls icons and coloring
